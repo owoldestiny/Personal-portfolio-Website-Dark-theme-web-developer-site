9035c33696c5db83d7a666e9bfd20dc6aab5d4af
Owol Destiny <74866792+owoldestiny@users.noreply.github.com>
2021-01-27 15:34:20 +0100

    Update main.js

--- Dark Personal Portfolio Website/js/main.js
+++ Dark Personal Portfolio Website/js/main.js
@@ -1,1 +1,23 @@
-// TYPE YOUR JS HERE
+$(document).ready(function() {
+    $('.menuIcon').click(function() {
+        $('body').toggleClass('menuShown');
+    });
+});
+
+$(document).ready(function() {
+    $("menu a").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000, function() {
+
+                window.location.hash = hash;
+            });
+        }
+    });
+});
